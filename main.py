d85f8629777218710cb0f2cc3d1d3004679be523
unknown <rohanjangotra03794@gmail.com>
2020-08-02 22:40:07 +0530

    'as'

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from azent import  create_app
+from azent import create_app
+
+app = create_app()
 
 if __name__ == '__main__':
-    create_app().run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
